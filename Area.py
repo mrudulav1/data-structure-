0f628da4002830735020f3096f6177c782bb0adf
Mrudula v <74678150+mrudulav1@users.noreply.github.com>
2020-12-21 12:28:21 +0530

    Delete Area.py

--- Area.py
+++
@@ -1,9 +0,0 @@
-#program to find area of square
-s=int(input("Enter the side:"))
-area=s*s
-print("Area of the square is :",area) #print area of square
-
-
-#Output
-Enter the side:6
-Area of the square is : 36
